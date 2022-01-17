f4dea7c178a8851f103eb883bbd23846a19043cc
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2022-01-17 15:03:14 -0400

    Made basic structs

--- OOP_Structs_Practice/Program.cs
+++ OOP_Structs_Practice/Program.cs
@@ -8,8 +8,30 @@ namespace OOP_Structs_Practice
 {
     class Program
     {
+        struct Vector2
+        {
+            public float x;
+            public float y;
+        }
+
+        struct Vector3
+        {
+            public float x;
+            public float y;
+            public float z;
+        }
+
         static void Main(string[] args)
         {
+            Vector2 gamepad;
+            Vector3 playerMovement;
+
         }
+
+        static void GetPlayerMovement()
+        {
+
+        }
+
     }
 }
